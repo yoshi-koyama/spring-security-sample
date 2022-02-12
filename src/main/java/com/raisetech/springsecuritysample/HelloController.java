c31d90878e0226ca8a59897b004eb5b9c3d379d1
小山由人 <koyama@reytech.co.jp>
2022-02-12 17:34:41 +0900

    login処理を追加

--- src/main/java/com/raisetech/springsecuritysample/HelloController.java
+++ src/main/java/com/raisetech/springsecuritysample/HelloController.java
@@ -1,13 +1,17 @@
 package com.raisetech.springsecuritysample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
         return "hello";
     }
 }
