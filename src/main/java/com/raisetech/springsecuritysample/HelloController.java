bc499d171ac73a76a23079b681dc2ba7ccbb501d
key <>
2022-02-16 01:41:09 +0900

    login処理を追加

--- src/main/java/com/raisetech/springsecuritysample/HelloController.java
+++ src/main/java/com/raisetech/springsecuritysample/HelloController.java
@@ -1,14 +1,17 @@
 package com.raisetech.springsecuritysample;
 
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
         return "hello";
     }
 }
